9d537804c24e9ec0f8676eccabdd248cf4a44a99
Alex Bieliakov <bieliakov.alex@gmail.com>
2020-01-01 20:33:05 +0200

    Add red_diode project
    Add app which should blink with red diode. There are code on C and
    Python (Python3).

---
+++ red_diode/red_diode.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+ledPin = 11
+
+def setup():
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(ledPin, GPIO.OUT)
+	GPIO.output(ledPin, GPIO.LOW)
+	print('using pin%d'%ledPin)
+	
+def loop():
+	while True:
+		GPIO.output(ledPin, GPIO.HIGH)
+		print('...led on')
+		time.sleep(1)
+
+def destroy():
+	GPIO.output(ledPin, GPIO.LOW)
+	GPIO.cleanup()
+	
+if __name__ == '__main__':
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:
+		destroy()
