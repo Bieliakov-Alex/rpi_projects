446c006df6748b8cdf4bfc801cd29669f5ef862d
Oleksandr Bieliakov <oleksandr.bieliakov@volvocars.com>
2020-01-02 16:51:41 +0200

    Fix codestyle for recent projects

--- button_diode/button_diode.c
+++ button_diode/button_diode.c
@@ -1,31 +1,29 @@
-#include <wiringPi.h>
 #include <stdio.h>
+#include <wiringPi.h>
 
 #define ledPin 0
 #define buttonPin 1
 
-int main(void)
-{
-	if(wiringPiSetup() == -1){
-		printf("Setup wiringPi failed!\n");
-		return 1;
-	}
-	
-	pinMode(ledPin, OUTPUT);
-	pinMode(buttonPin, INPUT);
-	
-	pullUpDnControl(buttonPin, PUD_UP);
-	
-	while(1){
-		if(digitalRead(buttonPin) == LOW){
-			digitalWrite(ledPin, HIGH);
-			printf("led on...\n");
-		}
-		else{
-			digitalWrite(ledPin, LOW);
-			printf("...led off\n");
-		}
-	}
-	
-	return 0;
+int main(void) {
+  if (wiringPiSetup() == -1) {
+    printf("Setup wiringPi failed!\n");
+    return 1;
+  }
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+
+  pullUpDnControl(buttonPin, PUD_UP);
+
+  while (1) {
+    if (digitalRead(buttonPin) == LOW) {
+      digitalWrite(ledPin, HIGH);
+      printf("led on...\n");
+    } else {
+      digitalWrite(ledPin, LOW);
+      printf("...led off\n");
+    }
+  }
+
+  return 0;
 }
--- red_diode/red_diode.c
+++ red_diode/red_diode.c
@@ -1,44 +1,42 @@
-#include <wiringPi.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <wiringPi.h>
 
 #define ledPin 0
 
 /* Signal Handler for SIGINT */
-void sigintHandler(int signal) 
-{ 
-    digitalWrite(ledPin, LOW);
-	printf("Turning off diode\n");
-	exit(1);
-} 
+void sigintHandler(int signal) {
+  digitalWrite(ledPin, LOW);
+  printf("Turning off diode\n");
+  exit(1);
+}
 
-int main(void)
-{
-	if(wiringPiSetup() == -1){
-		printf("setup wiringPi failed!\n");
-		return 1;
-	}
-	
-	printf("wiringPi initialize successfully, GPIO %d(wiringPi pin)\n", ledPin);
-	
-	if(signal(SIGINT, sigintHandler) == SIG_ERR){
-		printf("setup signal handler for Ctrl-C failed!\n");
-		return 1;
-	}
-	
-	printf("Setup signal handler for Ctrl-C succeded!\n");
-	
-	pinMode(ledPin, OUTPUT);
-	
-	while(1){
-		digitalWrite(ledPin, HIGH);
-		printf("led on...\n");
-		delay(1000);
-		digitalWrite(ledPin, LOW);
-		printf("... led off\n");
-		delay(1000);
-	}
-	
-	return 0;
+int main(void) {
+  if (wiringPiSetup() == -1) {
+    printf("setup wiringPi failed!\n");
+    return 1;
+  }
+
+  printf("wiringPi initialize successfully, GPIO %d(wiringPi pin)\n", ledPin);
+
+  if (signal(SIGINT, sigintHandler) == SIG_ERR) {
+    printf("setup signal handler for Ctrl-C failed!\n");
+    return 1;
+  }
+
+  printf("Setup signal handler for Ctrl-C succeded!\n");
+
+  pinMode(ledPin, OUTPUT);
+
+  while (1) {
+    digitalWrite(ledPin, HIGH);
+    printf("led on...\n");
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+    printf("... led off\n");
+    delay(1000);
+  }
+
+  return 0;
 }
